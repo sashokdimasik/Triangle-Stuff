b47f97f0905b09624119424ec084e922ca3db5ff
Alexander Platonov <144921734+sashokdimasik@users.noreply.github.com>
2023-09-27 16:31:18 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,11 @@
-/***********************************************
- * Author:       Platonov A.A.                 *
- * Group:        ПИ-231                        *
- * Variant:      14                            *
- * Date:         25.09.2023                    *
- * Project name: Triangle' Stuff               *
- * OnlineGDB: https://onlinegdb.com/ESdgCXpyT1 *
- ***********************************************/
+/**********************************************
+ * Author:       Platonov A.A.                *
+ * Group:        ПИ-231                       *
+ * Variant:      14                           *
+ * Date:         25.09.2023                   *
+ * Project name: Triangle' Stuff              *
+ * OnlineGDB: https://onlinegdb.com/B1VdRVgbZ *
+ **********************************************/
 
 #include <iostream>
 #include <iomanip>
@@ -14,24 +14,24 @@
 using namespace std;
 
 int main() {
-  double angleA, angleB, sideB, sideC, bisectorA, medianA, heightA;
+  double sideA, angleA, angleB, sideB, sideC, bisectorA, medianA, heightA;
   
-  double sideA = 0.0635;
+  cout << "Enter side A, angle A and angle B: ";
+  cin >> sideA >> angleA >> angleB;
   
-  angleA = 16 * M_PI / 180;
-  angleB = 44 * M_PI / 180;
-  
+  angleA *= M_PI / 180;
+  angleB *= M_PI / 180;
   sideB = sideA * sin(angleB) / sin(angleA);
   sideC = sideA * sin(M_PI - angleA - angleB) / sin(angleA);
   bisectorA = 2 * sideB * sideC * cos(angleA / 2) / (sideB + sideC);
   medianA = sqrt((sideB * sideB + sideC * sideC + 2 * sideB * sideC * cos(angleA)) / 2);
   heightA = sideC * sin(angleB);
   
-  cout << fixed << setprecision(6) << "Side B = " << sideB
-  << "\nSide C = " << sideC
-  << "\nBisector of A = " << bisectorA
-  << "\nMedian of A = "<< medianA
-  << "\nHeight from A = " << heightA << '\n';
+  cout << fixed << setprecision(6) << "\nSide B = " << sideB
+                                   << "\nSide C = " << sideC
+                                   << "\nBisector of A = " << bisectorA
+                                   << "\nMedian of A = "<< medianA
+                                   << "\nHeight from A = " << heightA << '\n';
   
   return 0;
 }
